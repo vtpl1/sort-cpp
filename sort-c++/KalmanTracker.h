dff94f7f6184fc1b18070ecac2957cd219ab5a9d
Monotosh Das <monotosh.das@videonetics.com>
2021-12-07 12:06:33 +0000

    t

--- sort-c++/KalmanTracker.h
+++ sort-c++/KalmanTracker.h
@@ -4,69 +4,62 @@
 #ifndef KALMAN_H
 #define KALMAN_H 2
 
-#include "opencv2/video/tracking.hpp"
 #include "opencv2/highgui/highgui.hpp"
+#include "opencv2/video/tracking.hpp"
 
 using namespace std;
 using namespace cv;
 
 #define StateType Rect_<float>
 
-
 // This class represents the internel state of individual tracked objects observed as bounding box.
 class KalmanTracker
 {
-public:
-	KalmanTracker()
-	{
-		init_kf(StateType());
-		m_time_since_update = 0;
-		m_hits = 0;
-		m_hit_streak = 0;
-		m_age = 0;
-		m_id = kf_count;
-		//kf_count++;
-	}
-	KalmanTracker(StateType initRect)
-	{
-		init_kf(initRect);
-		m_time_since_update = 0;
-		m_hits = 0;
-		m_hit_streak = 0;
-		m_age = 0;
-		m_id = kf_count;
-		kf_count++;
-	}
+  public:
+    KalmanTracker()
+    {
+        init_kf(StateType());
+        m_time_since_update = 0;
+        m_hits = 0;
+        m_hit_streak = 0;
+        m_age = 0;
+        m_id = kf_count;
+        // kf_count++;
+    }
+    KalmanTracker(StateType initRect)
+    {
+        init_kf(initRect);
+        m_time_since_update = 0;
+        m_hits = 0;
+        m_hit_streak = 0;
+        m_age = 0;
+        m_id = kf_count;
+        kf_count++;
+    }
 
-	~KalmanTracker()
-	{
-		m_history.clear();
-	}
+    ~KalmanTracker() { m_history.clear(); }
 
-	StateType predict();
-	void update(StateType stateMat);
-	
-	StateType get_state();
-	StateType get_rect_xysr(float cx, float cy, float s, float r);
+    StateType predict();
+    void update(StateType stateMat);
 
-	static int kf_count;
+    StateType get_state();
+    StateType get_rect_xysr(float cx, float cy, float s, float r);
 
-	int m_time_since_update;
-	int m_hits;
-	int m_hit_streak;
-	int m_age;
-	int m_id;
+    static int kf_count;
 
-private:
-	void init_kf(StateType stateMat);
+    int m_time_since_update;
+    int m_hits;
+    int m_hit_streak;
+    int m_age;
+    int m_id;
 
-	cv::KalmanFilter kf;
-	cv::Mat measurement;
+  private:
+    void init_kf(StateType stateMat);
 
-	std::vector<StateType> m_history;
+    cv::KalmanFilter kf;
+    cv::Mat measurement;
+
+    std::vector<StateType> m_history;
 };
-
-
-
 
 #endif
