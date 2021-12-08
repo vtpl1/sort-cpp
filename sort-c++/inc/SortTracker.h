e8b3f8365c6690d2120d2c863b6d574424dfc6cc
Monotosh Das <monotosh.das@videonetics.com>
2021-12-08 10:47:15 +0000

    headers moved in inc folder

--- sort-c++/inc/SortTracker.h
+++ sort-c++/inc/SortTracker.h
@@ -7,6 +7,8 @@
 namespace vtpl
 {
 typedef struct _TrackingBox {
+    _TrackingBox() {}
+    _TrackingBox(const cv::Rect_<float>& box): box(box) {}
     int frame{0};
     int id{0};
     cv::Rect_<float> box;
@@ -22,6 +24,7 @@ class SortTracker
     int _frame_count;
 
     std::vector<KalmanTracker> _trackers;
+
   public:
     SortTracker(int max_age, int min_hits, double iou_threshold, bool show_msg);
     std::vector<vtpl::TrackingBox> getResult(const std::vector<vtpl::TrackingBox>& tracking_box_vec, float rc_ext,
