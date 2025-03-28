bb18b33995251d0e0ef65997e67e1532f68bf41d
Monotosh Das <monotosh.das@videonetics.com>
2025-03-28 11:11:05 +0530

    wip

--- sort-cpp/include/Hungarian.h
+++ sort-cpp/include/Hungarian.h
@@ -15,8 +15,8 @@
 
 #include <iostream>
 #include <vector>
-
-class HungarianAlgorithm
+#include "sort_cpp_export.h"
+class SORT_CPP_EXPORT HungarianAlgorithm
 {
   public:
     HungarianAlgorithm() = default;
